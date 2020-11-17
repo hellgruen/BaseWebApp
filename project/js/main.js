b8541fe2d984620f733fcd3b11aad999cf8cd11a
Marie Blanke <hellgruen@gmx.de>
2020-11-18 00:20:20 +0100

    Added search field

--- project/js/main.js
+++ project/js/main.js
@@ -2,11 +2,16 @@ $(document).ready(function(){
   getWeather();
 })
 
-function getWeather(){
-  var url="https://api.openweathermap.org/data/2.5/weather?q=Berlin&units=metric&APPID="+apiKey;
+function getWeather(searchQuery){
+  var url="https://api.openweathermap.org/data/2.5/weather?q=+searchQuery+&units=metric&APPID="+apiKey;
 
   $.ajax(url, {success: function(data){
     $(".city").text(data.name);
     $(".temp").text(data.main.temp);
   }})
+}
+
+function searchWeather(){
+  var searchQuery = $(".search").val();
+  getWeather(searchQuery);
 }
