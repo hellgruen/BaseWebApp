86f1fdda22386eca3f6ab69036cd14ff98be02f4
Marie Blanke <hellgruen@gmx.de>
2020-11-17 22:24:58 +0100

    Added Celsius

--- project/js/main.js
+++ project/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 })
 
 function getWeather(){
-  var url="https://api.openweathermap.org/data/2.5/weather?q=Berlin&appid="+apiKey;
+  var url="https://api.openweathermap.org/data/2.5/weather?q=Berlin&units=metric&appid="+apiKey;
 
   $.ajax(url, {success: function(data){
     console.log(data);
